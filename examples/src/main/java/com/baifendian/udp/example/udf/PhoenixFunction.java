4c4744e3e72e339708baf62a86d02633a4f8e2e8
xiangyang.xu <xiangyang.xu@baifendian.com>
2017-09-13 14:35:17 +0800

    add phoenix function

--- examples/src/main/java/com/baifendian/udp/example/udf/PhoenixFunction.java
+++ examples/src/main/java/com/baifendian/udp/example/udf/PhoenixFunction.java
@@ -1,4 +1,5 @@
 package com.baifendian.udp.example.udf;
+
 import java.sql.SQLException;
 import java.util.List;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
@@ -6,23 +7,22 @@ import org.apache.phoenix.expression.Expression;
 import org.apache.phoenix.expression.function.ScalarFunction;
 import org.apache.phoenix.parse.FunctionParseNode.Argument;
 import org.apache.phoenix.parse.FunctionParseNode.BuiltInFunction;
-import org.apache.phoenix.schema.types.PLong;
-import org.apache.phoenix.schema.types.PVarchar;
 import org.apache.phoenix.schema.types.PDataType;
 import org.apache.phoenix.schema.tuple.Tuple;
+import org.apache.phoenix.schema.types.PVarchar;
 
 /**
  * 函数的输入参数为：bigInt，输入为: varchar
  */
-@BuiltInFunction(name=ReverseFunction.NAME,  args={
+@BuiltInFunction(name= PhoenixFunction.NAME,  args={
     @Argument(allowedTypes={PVarchar.class})} )
-public class ReverseFunction extends ScalarFunction {
-  public static final String NAME = "URLPARSE";
+public class PhoenixFunction extends ScalarFunction {
+  public static final String NAME = "HELLO";
 
-  public ReverseFunction() {
+  public PhoenixFunction() {
   }
 
-  public ReverseFunction(List children) throws SQLException {
+  public PhoenixFunction(List children) throws SQLException {
     super(children);
   }
 
@@ -33,33 +33,28 @@ public class ReverseFunction extends ScalarFunction {
 
   @Override
   public boolean evaluate(Tuple tuple, ImmutableBytesWritable ptr) {
-    Expression strExpression = getStrExpression();
-    if (!strExpression.evaluate(tuple, ptr)) {
+    // Get the child argument and evaluate it first
+    Expression arg = getChildren().get(0);
+    if (!arg.evaluate(tuple, ptr)) {
       return false;
     }
 
-    Long id = (Long) PLong.INSTANCE.toObject(ptr, strExpression.getSortOrder());
-
-    if (id == null) {
+    String sourceStr = (String)PVarchar.INSTANCE.toObject(ptr, arg.getSortOrder());
+    if (sourceStr == null) {
+      ptr.set(PVarchar.INSTANCE.toBytes("hello: null"));
       return true;
     }
-
-    ptr.set(PLong.INSTANCE.toBytes(id*2L));
-
+    ptr.set(PVarchar.INSTANCE.toBytes("hello: "+sourceStr));
     return true;
   }
 
   @Override
   public PDataType getDataType() {
-    return getStrExpression().getDataType();
+    return getChildren().get(0).getDataType();
   }
 
   @Override
-  public boolean isNullable() {
-    return getStrExpression().isNullable();
-  }
-
-  private Expression getStrExpression() {
-    return children.get(0);
+ public boolean isNullable() {
+    return getChildren().get(0).isNullable();
   }
 }
